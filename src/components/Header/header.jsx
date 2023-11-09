87f9ce7799455c8aa00b05d866ccf42a96b8c13b
daviguerra05 <rm551605@fiap.com.br>
2023-11-09 20:14:19 -0300

    rotas

--- src/components/Header/header.jsx
+++ src/components/Header/header.jsx
@@ -1,18 +1,35 @@
 import './Header.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { NavLink } from 'react-router-dom';
 
 function Header(){
     return( 
         <header className=' p-2 bg-dark'>
+
             <div className='m-3'>
-                <a className='text-decoration-none text-white' href='#home'>CLARKE</a>
+                <NavLink to="/" className='text-decoration-none text-white' href='#home'>
+                    CLARKE
+                </NavLink>
             </div>
 
             <div id='nav'> 
-            <div className='m-3'><a className='text-decoration-none text-white' href='#solucoes'>Tecnologias</a></div>
-            <div className='m-3'><a className='text-decoration-none text-white' href='#footer'>Sobre Nós</a></div>
-            <div className='m-3'><a className='text-decoration-none text-white' href='#login'>Comprar</a></div> 
+                <div className='m-3'>
+                    <NavLink to="/Tecnologias" className='text-decoration-none text-white' href='#solucoes'>
+                        Tecnologias
+                    </NavLink>
+                </div>
+                <div className='m-3'>
+                    <NavLink to="/Sobrenos" className='text-decoration-none text-white' href='#footer'>
+                        Sobre Nós
+                    </NavLink>
+                </div>
+                <div className='m-3'>
+                    <NavLink to="/Compras" className='text-decoration-none text-white' href='#login'>
+                        Comprar
+                    </NavLink>
+                </div> 
             </div>
+            
         </header>
     )
 } 
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,31 @@
-import React from 'react'
+import React, { } from 'react'
 import ReactDOM from 'react-dom/client'
+/* importando as dependencias do browserRouter e Router Provider*/
+import { createBrowserRouter, RouterProvider} from 'react-router-dom'
+/* importando os elementos*/
 import App from './App.jsx'
+import Error from './Error.jsx'
+import Home from './components/Home/home.jsx'
 import './css/index.css'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+ /* criando a função createBrowser */
+ const router = createBrowserRouter([
+  {
+     /* chamando o elemento principal(pai) */
+    path: '/', element: <App />,
+     /*  chamando o elemento de erro*/
+    errorElement: <Error />,
+  
+     /* chamando elementos filhos */
+  children:[
+    {path: '/', element:<Home />},
+  ]
+  }
+  ])
+
+  ReactDOM.createRoot(document.getElementById('root')).render(
+    <React.StrictMode>
+       {/*chamando a função que será renderizada */}
+      <RouterProvider router={router} />
+    </React.StrictMode>,
+  )
