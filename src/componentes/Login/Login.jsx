7d97ca6d0914d5e9b34974ce7615b2eb16e1ebba
daviguerra05 <davi@passanha.com.br>
2023-11-11 18:46:31 -0300

    styled

--- src/componentes/Login/Login.jsx
+++ src/componentes/Login/Login.jsx
@@ -1,6 +1,40 @@
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import styled from 'styled-components';
+
+const Fd = styled.div`
+    width:100%;
+    display: flex;
+    padding-top: 50%;
+    align-items:center;
+    justify-content:center;
+    Form{
+        width:50%;
+    }
+`
+
 export default function Login(){
     return(
         <>
+            <Fd>
+                <Form>
+                    <Form.Group className="mb-3" controlId="formBasicEmail">
+                        <Form.Label>Endereço de email</Form.Label>
+                        <Form.Control type="email" placeholder="Digite seu email" />
+                        <Form.Text className="text-muted">
+                        </Form.Text>
+                    </Form.Group>
+
+                    <Form.Group className="mb-3" controlId="formBasicPassword">
+                        <Form.Label>Senha</Form.Label>
+                        <Form.Control type="password" placeholder="Digite sua senha" />
+                    </Form.Group>
+
+                    <Button variant="primary" type="submit">
+                        Submit
+                    </Button>
+                </Form>
+            </Fd>
         </>
     )
 }
