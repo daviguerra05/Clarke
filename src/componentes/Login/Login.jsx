18990b9231d6e9105a07dfe805b7782c4049b14c
daviguerra05 <davi@passanha.com.br>
2023-11-11 18:48:37 -0300

    a

--- src/componentes/Login/Login.jsx
+++ src/componentes/Login/Login.jsx
@@ -4,8 +4,8 @@ import styled from 'styled-components';
 
 const Fd = styled.div`
     width:100%;
+    height:100vh;
     display: flex;
-    padding-top: 50%;
     align-items:center;
     justify-content:center;
     Form{
